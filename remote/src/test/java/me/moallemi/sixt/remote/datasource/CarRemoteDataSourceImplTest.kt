302cee79483b635fa5c403e08cb547e55694bce0
Reza Moallemi <rm.allmails@gmail.com>
2019-05-06 09:02:24 +0430

    Add test for CarRemoteDataSource

---
+++ remote/src/test/java/me/moallemi/sixt/remote/datasource/CarRemoteDataSourceImplTest.kt
@@ -0,0 +1,37 @@
+package me.moallemi.sixt.remote.datasource
+
+import io.mockk.every
+import io.mockk.mockk
+import io.reactivex.Single
+import me.moallemi.sixt.remote.api.CarService
+import me.moallemi.sixt.remote.dto.toCarEntity
+import org.junit.Test
+import java.io.IOException
+
+class CarRemoteDataSourceImplTest {
+
+    private val service: CarService = mockk()
+    private val dataSource = CarRemoteDataSourceImpl(service)
+
+    @Test
+    fun `getCars when successful`() {
+        every { service.getCars() } returns Single.just(listOf(CarFactory.createCarDto()))
+
+        dataSource.getCars()
+            .test()
+            .assertSubscribed()
+            .assertComplete()
+            .assertNoErrors()
+            .assertValue(listOf(CarFactory.createCarDto().toCarEntity()))
+    }
+
+    @Test
+    fun `getCars when fails`() {
+        every { service.getCars() } returns Single.error(IOException())
+
+        dataSource.getCars()
+            .test()
+            .assertNotComplete()
+            .assertError(IOException::class.java)
+    }
+}
