081e52b6cdb3b26c1a0b4ff3cc7c26b815d69fd4
Reza Moallemi <rm.allmails@gmail.com>
2019-05-06 10:34:02 +0430

    Add test for data module

---
+++ data/src/test/java/me/moallemi/sixt/data/repository/CarFactory.kt
@@ -0,0 +1,25 @@
+package me.moallemi.sixt.data.repository
+
+import me.moallemi.sixt.data.entity.CarEntity
+
+object CarFactory {
+
+    fun createCarEntity() = CarEntity(
+        id = "WMWSW31030T222518",
+        modelIdentifier = "mini",
+        modelName = "MINI",
+        name = "Vanessa",
+        make = "BMW",
+        group = "MINI",
+        color = "midnight_black",
+        series = "MINI",
+        fuelType = "D",
+        fuelLevel = 0.7,
+        transmission = "M",
+        licensePlate = "M-VO0259",
+        latitude = 48.134557,
+        longitude = 11.576921,
+        innerCleanliness = "REGULAR",
+        carImageUrl = "https://cdn.sixt.io/codingtask/images/mini.png"
+    )
+}
---
+++ data/src/test/java/me/moallemi/sixt/data/repository/CarRepositoryImplTest.kt
@@ -0,0 +1,44 @@
+package me.moallemi.sixt.data.repository
+
+import io.mockk.clearAllMocks
+import io.mockk.every
+import io.mockk.mockk
+import io.reactivex.Single
+import me.moallemi.sixt.data.datasource.CarRemoteDataSource
+import me.moallemi.sixt.data.entity.toCar
+import org.junit.Before
+import org.junit.Test
+import java.io.IOException
+
+class CarRepositoryImplTest {
+
+    private val carRemoteDataSource: CarRemoteDataSource = mockk()
+    private val carRepository = CarRepositoryImpl(carRemoteDataSource)
+
+    @Before
+    fun setUp() {
+        clearAllMocks()
+    }
+
+    @Test
+    fun `getCars when successful`() {
+        val carEntity = CarFactory.createCarEntity()
+        every { carRemoteDataSource.getCars() } returns Single.just(listOf(carEntity))
+
+        carRepository.getCars()
+            .test()
+            .assertComplete()
+            .assertNoErrors()
+            .assertValue(listOf(carEntity.toCar()))
+    }
+
+    @Test
+    fun `getCars when fails`() {
+        every { carRemoteDataSource.getCars() } returns Single.error(IOException())
+
+        carRepository.getCars()
+            .test()
+            .assertNotComplete()
+            .assertFailure(IOException::class.java)
+    }
+}
